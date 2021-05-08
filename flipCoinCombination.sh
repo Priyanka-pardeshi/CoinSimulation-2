ad5d3a4422b81ebace0163e2f50000f359d455dd
echo # <priyankapardeshi2312@gmail.com>
2021-05-09 01:43:24 +0530

    Calculated persentages of head and tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,12 +1,34 @@
 #! /bin/bash -x
+read -p "how many times wanna toss coin? " n
+#declaring an arrary to save result of toss.
+declare -a arr1
 
-toss=$((1+RANDOM%2))
-if (($toss==1))
-then
-        echo "Its Head"
+#declaring count variable to trass number of head and tail.
+countH=0
+countT=0
+tossCount=0
+
+for ((i=1; i<=n;i++))
+do
+        toss=$((1+RANDOM%2))
+        ((tossCount++))
+        if (($toss==1))
+        then
+                arr1[$i]="Head"
+                ((countH++))
         elif (($toss==2))
         then
-                echo "Its Tail"
+                arr1[i]="Tail"
+                ((countT++))
         else
-                echo "invalid "
-fi
+                echo "invalid number"
+        fi
+done
+echo $tossCount $countH $countT
+
+per=`echo $tossCount $countH | awk '{print $2*100/$1}'`
+echo " The persentage of head  is $per %"
+echo " The persentage of tail is $((100-$per))%"
+
+
+

98b4c449d7b7a545974c4965fe597ad1ba1d5084
echo # <priyankapardeshi2312@gmail.com>
2021-05-09 01:57:06 +0530

    Three coins are toss and checks Probability

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,49 +3,86 @@ read -p "how many times wanna toss coin? " n
 #declaring an arrary to save result of toss.
 declare -a arr1
 
-#declaring count variable to trass number of head and tail.
-countHH=0
-countHT=0
-countTH=0
-countTT=0
+#declaring count variable to trace number of head and tail.
+countHHH=0
+countHHT=0
+countHTH=0
+countHTT=0
+countTHH=0
+countTHT=0
+countTTH=0
+countTTT=0
 tossCount=0
 
 for ((i=1; i<=n;i++))
 do
         toss1=$((1+RANDOM%2))
         toss2=$((1+RANDOM%2))
+        toss3=$((1+RANDOM%2))
         ((tossCount++))
-        if (($toss1==1 & $toss2==1 ))
+        if (($toss1==1 & $toss2==1 & $toss3==1))
         then
-                arr1[$i]="HH"
-                ((countHH++))
-        elif (($toss1==1 & $toss2==2))
+                arr1[$i]="HHH"
+                ((countHHH++))
+        elif (($toss1==1 & $toss2==1 & $toss3==2))
         then
-                arr1[i]="HT"
-                ((countHT++))
-        elif (($toss1==2 & $toss2==1))
+                arr1[$i]="HHT"
+                ((countHHT++))
+        elif (($toss1==1 & $toss2==2 & $toss3==1))
         then
-                arr1[$i]="TH"
-                ((countTH++))
-        elif (($toss1==2 & $toss2==2))
+                arr1[$i]="HTH"
+                ((countHTH++))
+
+        elif (($toss1==1 & $toss2==2 & $toss3==2))
         then
-                arr1[$i]="TT"
-                ((countTT++))
+                arr1[$i]="HTT"
+                ((countHTT++))
+        elif (($toss1==2 & $toss2==1 & $toss3==1))
+        then
+                arr1[$i]="THH"
+                ((countTHH++))
+        elif (($toss1==2 & $toss2==1 & $toss3==2))
+        then
+                arr1[$i]="THT"
+                ((countTHT++))
+        elif (($toss1==2 & $toss2==2 & $toss3==1))
+        then
+                arr1[$i]="TTH"
+                ((countTTH++))
+        elif (($toss1==2 & $toss2==2 & $toss3==2))
+        then
+                arr1[$i]="TTT"
+                ((countTTT++))
+
         fi
+
 done
-echo "total=$tossCount , HH=$countHH , HT=$countHT , TH=$countTH  TT=$countTT"
-#persentage=`echo $tossCount $countH | awk '{print $2*100/$1}'`
-HH=`echo $tossCount $countHH | awk '{print $2*100/$1}'`
-echo "persentage of HH is $HH"
+echo "total=$tossCount , HHH=$countHHH , HHT=$countHHT , HTH=$countHTH  HTT=$countHTT"
+echo "THH=$countTHH THT=$countTHT TTH=$countTTH TTT=$countTTT "
 
-HT=`echo $tossCount $countHT | awk '{print $2*100/$1}'`
-echo "persentage of HT is $HT"
+HHH=`echo $tossCount $countHHH | awk '{print $2*100/$1}'`
+echo "persentage of HHH is $HHH"
 
-TH=`echo $tossCount $countTH | awk '{print $2*100/$1}'`
-echo "persentage of TH is $TH"
+HHT=`echo $tossCount $countHHT | awk '{print $2*100/$1}'`
+echo "persentage of HHT is $HHT"
 
-TT=`echo $tossCount $countTT | awk '{print $2*100/$1}'`
-echo "persentage of TT is $TT"
+HTH=`echo $tossCount $countHTH | awk '{print $2*100/$1}'`
+echo "persentage of HTH is $HTH"
+
+HTT=`echo $tossCount $countHTT | awk '{print $2*100/$1}'`
+echo "persentage of HTT is $HTT"
+
+THH=`echo $tossCount $countTHH | awk '{print $2*100/$1}'`
+echo "persentage of THH is $THH"
+
+THT=`echo $tossCount $countTHT | awk '{print $2*100/$1}'`
+echo "persentage of THH is $THT"
+
+TTH=`echo $tossCount $countTTH | awk '{print $2*100/$1}'`
+echo "persentage of TTH is $TTH"
+
+TTT=`echo $tossCount $countTTT | awk '{print $2*100/$1}'`
+echo "persentage of TTT is $TTT"
 
 
 
